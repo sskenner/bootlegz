84fcad31ec8529eb0aee98c93148cd760c2d7cef
sskenner <sskenner@gmail.com>
2013-10-19 22:16:36 -0400

    implement user can review a video feature

--- spec/controllers/videos_controller_spec.rb
+++ spec/controllers/videos_controller_spec.rb
@@ -2,25 +2,40 @@ require 'spec_helper'
 
 describe VideosController do
   describe "Get show" do
-    context "with authenticated users" do
-      before do
-        session[:user_id] = Fabricate(:user).id
-      end
+    it "sets @video for authenticated users" do
+      session[:user_id] = Fabricate(:user).id
+      video = Fabricate(:video)
+      get :show, id: video.id
+      expect(assigns(:video)).to eq(video)
+    end
 
-      it "sets @video" do
-        video = Fabricate(:video)
-        get :show, id: video.id
-        expect(assigns(:video)).to eq(video)
-      end
+    it "sets @reviews for authenticated users" do
+      session[:user_id] = Fabricate(:user).id
+      video = Fabricate(:video)
+      review1 = Fabricate(:review, video: video)
+      review2 = Fabricate(:review, video: video)
+      get :show, id: video.id
+      expect(assigns(:reviews)).to match_array([review1, review2])
+    end
 
-      # testing against rails convention
-      # it "renders the show template" do
-      #   video = Fabricate(:video)
-      #   get :show, id: video.id
-      #   expect(response).to render_template :show
-      # end
+    it "redirects the user to the sign in page for unauthenticated users" do
+      video = Fabricate(:video)
+      get :show, id: video.id
+      expect(response).to redirect_to sign_in_path
+    end
+  end
 
-      context
+  describe "POST search" do
+    it "sets @results for authenticated users" do
+      session[:user_id] = Fabricate(:user).id
+      futurama = Fabricate(:video, title: 'Futurama')
+      post :search, search_term: 'rama'
+      expect(assigns(:results)).to eq([futurama])
+    end
+    it "redirects to sign in page for the unauthenticated users" do
+      futurama = Fabricate(:video, title: 'Futurama')
+      post :search, search_term: 'rama'
+      expect(response).to redirect_to sign_in_path
     end
   end
 end
